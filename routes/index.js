6439e448ec979ab87c76257a6b934363ceb4dc93
fiorittoev <fiorittoev@gmail.com>
2024-03-09 22:58:16 -0500

    User values

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var router = require('express').Router();
 const request = require('request');
 const { requiresAuth } = require('express-openid-connect');
 const uploadedUsernames = new Set();
+loadFromKintone();
 
 router.get('/', function (req, res, next) {
   res.render('index', {
@@ -12,7 +13,6 @@ router.get('/', function (req, res, next) {
 
 router.get('/profile', requiresAuth(), function (req, res, next) {
   const nick = req.oidc.user.nickname;
-
   uploadToKintone(nick);
   res.render('profile', {
     userProfile: JSON.stringify(req.oidc.user, null, 2),
@@ -26,15 +26,56 @@ router.get('/leaderboard', requiresAuth(), function (req, res, next) {
 
 module.exports = router;
 
+
+function loadFromKintone() {
+  // Kintone API endpoint
+  const kintoneEndpoint = 'https://carboncount.kintone.com/k/v1/records.json';
+
+  // Kintone app ID from where you want to load the data
+  const appId = '1';
+
+  // Kintone API token for authentication
+  const apiToken = 'A8mvHRY5yrVQhIkcTSDW9amw4bX3J4XfXv3U2cLm';
+
+  // Make a GET request to load data from Kintone
+  request.get({
+      url: kintoneEndpoint,
+      headers: {
+          'X-Cybozu-API-Token': apiToken
+      },
+      qs: {
+          app: appId
+      },
+      json: true
+  }, (error, response, body) => {
+      if (error) {
+          console.error('Error loading data from Kintone:', error);
+          return;
+      }
+
+      // Process the response body
+      if (body && body.records) {
+          // Iterate over each record and do something with the data
+          body.records.forEach(record => {
+              console.log('Username loaded from Kintone:', record.username.value);
+              uploadedUsernames.add(record.username.value);
+          });
+      } else {
+          console.log('No records found in Kintone.');
+      }
+  });
+}
+
 //Function to upload username to Kintone
 function uploadToKintone(nick) {
+  console.log(uploadedUsernames);
   if (uploadedUsernames.has(nick)) {
     console.log('Username already uploaded to Kintone:', nick);
     return;
   }
   console.log('Uploading username to Kintone:', nick);
     // Kintone API endpoint
-    const kintoneEndpoint = 'https://carboncount.kintone.com/k/v1/record.json';
+    const kintoneEndpoint = 'https://carboncount.kintone.com/k/v1/records.json';
 
     // Kintone app ID where you want to upload the data
     const appId = '1';
@@ -45,11 +86,24 @@ function uploadToKintone(nick) {
     // Data to be uploaded
     const data = {
         'app': appId,
-        'record':
+        'records':
             {
                 'username': {
                   'value': nick
+                },
+                'trips':{
+                  'value': '0'
+                },
+                'miles':{
+                  'value': '0'
+                },
+                'emissions':{
+                  'value': '0'
+                },
+                'points':{
+                  'value': '0'
                 }
+
             }
     };
 
@@ -68,7 +122,7 @@ function uploadToKintone(nick) {
             return;
         }
         console.log('Data uploaded to Kintone:', body);
-      uploadedUsernames.add(nick);
+      
     });
 }
 
